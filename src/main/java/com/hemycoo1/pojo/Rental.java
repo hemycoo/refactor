1b2f754a959596b7d7674df119a951bd52612d73
hemycoo <hl106354432@outlook.com>
2022-04-25 22:30:42 +0800

    2.2 搬移函数

--- src/main/java/com/hemycoo1/pojo/Rental.java
+++ src/main/java/com/hemycoo1/pojo/Rental.java
@@ -10,4 +10,24 @@ import lombok.NoArgsConstructor;
 public class Rental {
     private Movie movie;
     private int daysRented;
+
+    public double getCharge() {
+        double thisAmount = 0.00;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2)
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3)
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
