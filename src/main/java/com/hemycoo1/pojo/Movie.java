ea3cf68c0571dd629a8b5d294bf9669d91a31c4c
hemycoo <1063854432@qq.com>
2022-04-25 14:56:24 +0800

    初始案例

---
+++ src/main/java/com/hemycoo1/pojo/Movie.java
@@ -0,0 +1,13 @@
+package com.hemycoo1.pojo;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+}
---
+++ src/main/java/com/hemycoo1/pojo/Rental.java
@@ -0,0 +1,9 @@
+package com.hemycoo1.pojo;
+
+import lombok.Data;
+
+@Data
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+}
