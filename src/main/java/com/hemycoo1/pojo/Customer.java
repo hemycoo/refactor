b37baff282b6251055f7be26b947cd6e435c8806
hemycoo <hl106354432@outlook.com>
2022-04-25 22:50:15 +0800

    2.4 优化多余的变量thisAmount

--- src/main/java/com/hemycoo1/pojo/Customer.java
+++ src/main/java/com/hemycoo1/pojo/Customer.java
@@ -20,18 +20,15 @@ public class Customer {
         Enumeration rentals = getRentalVector().elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()){
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + totalAmount + "\n";
