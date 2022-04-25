6ac083ae6730adac0cf45ba1f75a959f0c519219
hemycoo <hl106354432@outlook.com>
2022-04-25 22:39:55 +0800

    2.3 优化

--- src/main/java/com/hemycoo1/pojo/Customer.java
+++ src/main/java/com/hemycoo1/pojo/Customer.java
@@ -23,7 +23,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
             //determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -37,9 +37,5 @@ public class Customer {
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
-    }
-
-    private double amountFor(Rental rental){
-        return rental.getCharge();
     }
 }
