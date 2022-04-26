3b14b7d4d3ec3d4e916135d3e3f027dd91f553d7
hemycoo <1063854432@qq.com>
2022-04-26 14:28:31 +0800

    2.7 移动point到一个新函数

--- src/main/java/com/hemycoo1/pojo/Customer.java
+++ src/main/java/com/hemycoo1/pojo/Customer.java
@@ -15,20 +15,16 @@ public class Customer {
     private Vector<Rental> rentalVector = new Vector();
 
     public String statement(){
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentals = getRentalVector().elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()){
             Rental each = (Rental) rentals.nextElement();
-            // add frequent renter points
-            frequentRenterPoints += each.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
         }
         //add footer lines
         result += "Amount owed is " + getTotalCharge() + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
         return result;
     }
 
@@ -38,6 +34,16 @@ public class Customer {
         while (rentals.hasMoreElements()){
             Rental each = (Rental) rentals.nextElement();
             result += each.getCharge();
+        }
+        return result;
+    }
+
+    public int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = getRentalVector().elements();
+        while (rentals.hasMoreElements()){
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }
