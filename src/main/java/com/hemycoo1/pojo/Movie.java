2ec1dff6bb35f12f6a8de13d65f21ae512e39d72
hemycoo <1063854432@qq.com>
2022-04-26 16:21:38 +0800

    2.8 将switch移动到movie下面

--- src/main/java/com/hemycoo1/pojo/Movie.java
+++ src/main/java/com/hemycoo1/pojo/Movie.java
@@ -14,4 +14,24 @@ public class Movie {
 
     private String title;
     private int priceCode;
+
+    public double getCharge(int daysRented) {
+        double thisAmount = 0.00;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
