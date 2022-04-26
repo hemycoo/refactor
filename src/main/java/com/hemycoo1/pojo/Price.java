b2cbdc289e3088591c6b7c5f23e69b6c019778f8
hemycoo <hl106354432@outlook.com>
2022-04-26 23:01:07 +0800

    2.10.4 对point同样处理

--- src/main/java/com/hemycoo1/pojo/Price.java
+++ src/main/java/com/hemycoo1/pojo/Price.java
@@ -3,4 +3,8 @@ package com.hemycoo1.pojo;
 public abstract class Price {
     abstract int getPriceCode();
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
