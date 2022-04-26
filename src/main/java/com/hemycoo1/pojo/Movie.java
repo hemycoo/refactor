33ed8e7477833033ff0a07071e98a47ed2595729
hemycoo <hl106354432@outlook.com>
2022-04-26 22:42:12 +0800

    2.10.2 对getCharge()实施Move Method(142).

--- src/main/java/com/hemycoo1/pojo/Movie.java
+++ src/main/java/com/hemycoo1/pojo/Movie.java
@@ -16,23 +16,7 @@ public class Movie {
     private Price price;
 
     public double getCharge(int daysRented) {
-        double thisAmount = 0.00;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
